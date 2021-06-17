a62841976a8b122a90f8ed47852f008bc23ebac6
abhinav-sjsu <abhinav.kapur1408@gmail.com>
2021-06-16 19:28:32 -0700

    Adding TicketingApplication class

--- printTicketService/src/main/java/com/letsstartcoding/springbootrestapiexample/TicketingApplication.java
+++ printTicketService/src/main/java/com/letsstartcoding/springbootrestapiexample/TicketingApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class EmployeeApplication {
+public class TicketingApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeApplication.class, args);
+		SpringApplication.run(TicketingApplication.class, args);
 	}
 
 }
